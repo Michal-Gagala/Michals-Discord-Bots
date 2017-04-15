821632f913a7ba9e5cf20ffb2c622a27d92fa85a
Michal Gagala <mgagala57@gmail.com>
2017-04-15 21:11:11 +0100

    Documentation
    Commented the code a bit, should be mostly self explanatory

--- mc.py
+++ mc.py
@@ -2,16 +2,16 @@ import discord
 from time import sleep
 from datetime import datetime
 import requests
-from mcstatus import MinecraftServer
-from threading import Thread 
+from mcstatus import MinecraftServer 
+from threading import Thread # To alert for server status
 
-cid ='287712221751148555'
-ip='162.244.167.111:32252'
-server = MinecraftServer.lookup(ip)
-token = ''
-client = discord.Client()
+cid ='287712221751148555'  # Text channel id, to get this, simply enable developer mode in discord, right click the channel and copy id
+ip='xxxxx' # Server IP with the port, so: 'xxx.xxx.xxx.xxx:yyyyy'
+server = MinecraftServer.lookup(ip) 
+token = '' # Bot's token
+client = discord.Client() # Discord client
 
-p=True
+p=True # Helper var for active server monitoring
 
 
 info = """
@@ -21,29 +21,25 @@ info = """
 def alert(client, server, p):
     np = p
     while True:
-        sleep(120)
+        sleep(120) # To not kill internet connection
         try:
-            p = server.ping()
+            p = server.ping() # If this works, server is on
         except:
-            p=0
+            p=0 #Otherwise it has to be off
 
-        if p:
+        if p:  # If there's a ping value, the connection is on
             p=True
         else:
-            p=False
+            p=False #Otherwise it's off
 
-        if p != np:
+        if p != np: #If status has changed, inform the user
             if not p:
                 client.send_message(discord.Object(id=cid), 'Oh no, it seems the server is down!')
                 print('{0} :- Alerting that server is offline'.format(datetime.now()))
             else:
                 client.send_message(discord.Object(id=cid), 'YAY, it\'s back up!')
                 print('{0} :- Alerting that server is back up'.format(datetime.now()))
-            np = p                
-
-
-
-
+            np = p  # Reset the 2 vars         
 
 
 @client.event
@@ -83,6 +79,6 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    thread = Thread(target=alert, args=(client, server, p))
+    thread = Thread(target=alert, args=(client, server, p)) # Starts the active alert system
     thread.start()
 client.run(token)
